2865b13d12ea6822b9236a2713d96a6219c135ab
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-21 18:13:58 -0300

    useState - counter

---
+++ 01-hooks-and-adv-topics/src/tutorial/01-useState/5-useState-counter.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+export const UseStateCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const decrease = () => {
+    if (counter === 0) return;
+    setCounter(counter - 1);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setCounter(value + 1);
+      setCounter(prevState => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={() => setCounter(counter + 1)}>
+          increase
+        </button>
+        <button className="btn" onClick={decrease}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setCounter(0)}>
+          reset
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
+  );
+};
