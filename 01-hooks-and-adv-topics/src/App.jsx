7d69129fa147a10f06920373ec807568aab30fc9
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-21 14:49:28 -0300

    useState - basic

---
+++ 01-hooks-and-adv-topics/src/App.jsx
@@ -0,0 +1,12 @@
+// import { ErrorExample } from './tutorial/1-useState/setup/1-error-example';
+import { UseStateBasics } from './tutorial/1-useState/setup/2-useState-basics';
+
+function App() {
+  return (
+    <div className="container">
+      <UseStateBasics />
+    </div>
+  );
+}
+
+export default App;
