da96c76fc6665c4343dcccf37887b1ebfb9ead0d
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-21 15:29:42 -0300

    useState - Object example

--- 01-hooks-and-adv-topics/src/App.jsx
+++ 01-hooks-and-adv-topics/src/App.jsx
@@ -1,11 +1,12 @@
 // import { ErrorExample } from './tutorial/1-useState/setup/1-error-example';
 // import { UseStateBasics } from './tutorial/1-useState/setup/2-useState-basics';
-import { UseStateArray } from './tutorial/1-useState/setup/3-useState-array';
+// import { UseStateArray } from './tutorial/1-useState/setup/3-useState-array';
+import { UseStateObject } from './tutorial/1-useState/setup/4-useState-object';
 
 function App() {
   return (
     <div className="container">
-      <UseStateArray />
+      <UseStateObject />
     </div>
   );
 }
--- 01-hooks-and-adv-topics/src/tutorial/1-useState/setup/4-useState-object.jsx
+++ 01-hooks-and-adv-topics/src/tutorial/1-useState/setup/4-useState-object.jsx
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+export const UseStateObject = () => {
+  const [people, setPeople] = useState({
+    username: 'Ryan Gosling',
+    age: 24,
+    message: "I'm Ryan Gosling, what's up?",
+  });
+  const { username, message, age } = people;
+
+  const changeMessage = () => {
+    setPeople({
+      ...people,
+      username: ' Brene Brown',
+      message: '“Háblate a ti mismo como harías con alguien a quien amas.”',
+    });
+  };
+
+  return (
+    <>
+      <h3>{username}</h3>
+      <h4>{message}</h4>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
-
-export default UseStateObject;
