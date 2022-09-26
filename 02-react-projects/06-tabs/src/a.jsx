9e5a9e624a89e88af835c6f67bd58c4024001dca
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-26 18:11:54 -0300

    agregando fetch data

--- 02-react-projects/06-tabs/src/a.jsx
+++ 02-react-projects/06-tabs/src/a.jsx
@@ -1,31 +1,29 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
-
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project';
-
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newJobs = await response.json();
+    const reponse = await fetch(url);
+    const newJobs = await reponse.json();
     setJobs(newJobs);
-    setIsLoading(false);
+    setLoading(false);
   };
   useEffect(() => {
     fetchJobs();
   }, []);
-
-  if (isLoading) {
+  if (loading) {
     return (
       <section className="section loading">
         <h1>Loading...</h1>
       </section>
     );
   }
-
   const { company, dates, duties, title } = jobs[value];
   return (
     <section className="section">
@@ -33,14 +31,13 @@ function App() {
         <h2>experience</h2>
         <div className="underline"></div>
       </div>
-
       <div className="jobs-center">
         {/* btn container */}
         <div className="btn-container">
           {jobs.map((item, index) => {
             return (
               <button
-                key={index}
+                key={item.id}
                 onClick={() => setValue(index)}
                 className={`job-btn ${index === value && 'active-btn'}`}
               >
@@ -57,7 +54,7 @@ function App() {
           {duties.map((duty, index) => {
             return (
               <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon" />
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
                 <p>{duty}</p>
               </div>
             );
