e734f47e33ca64b8aa0faaedcbb6d358fcc7f203
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-26 17:47:37 -0300

    agregando fetch data

--- 02-react-projects/06-tabs/src/App.jsx
+++ 02-react-projects/06-tabs/src/App.jsx
@@ -4,6 +4,29 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const data = response.json();
+    setJobs(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
   return <h2>tabs project setup</h2>;
 }
 
