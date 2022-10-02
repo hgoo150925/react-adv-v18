9f35f10c09296e4ddbb385b7b7257021e8d3afdb
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-02 20:42:49 -0300

    controlled inputs

--- 01-hooks-and-adv-topics/src/tutorial/04-forms/1-controlled-inputs.jsx
+++ 01-hooks-and-adv-topics/src/tutorial/04-forms/1-controlled-inputs.jsx
@@ -6,9 +6,12 @@ import { useState } from "react";
 // value, onChange
 
 export const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("first");
+    alert(firstName);
   }
 
   return (
@@ -17,15 +20,29 @@ export const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name: </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
           </div>
 
           <div className="form-control">
             <label htmlFor="email">Email: </label>
-            <input type="text" id="email" name="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
 
-          <button type="submit">add person</button>
+          <button type="submit" onClick={handleSubmit}>
+            add person
+          </button>
         </form>
       </article>
     </>
