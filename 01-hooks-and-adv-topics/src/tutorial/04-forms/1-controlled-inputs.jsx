b9cb520c5d9f0649f6eba68062bab1ed66ee65bf
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-02 20:28:23 -0300

    controlled inputs

--- 01-hooks-and-adv-topics/src/tutorial/04-forms/1-controlled-inputs.jsx
+++ 01-hooks-and-adv-topics/src/tutorial/04-forms/1-controlled-inputs.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,5 +6,28 @@ import { useState } from 'react';
 // value, onChange
 
 export const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("first");
+  }
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input type="text" id="email" name="email" />
+          </div>
+
+          <button type="submit">add person</button>
+        </form>
+      </article>
+    </>
+  );
 };
