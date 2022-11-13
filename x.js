bbedad52cc84b6a34fbd4f07bc439f17b5f89644
hugoorlando <hugo.orlando2030@gmail.com>
2022-11-13 19:54:08 -0300

    se eliminaron archivos

--- x.js
+++
@@ -1,8 +0,0 @@
-const amorAPapa = false;
-const honrarPadres = true;
-const guardarShabbat = true;
-const diezmar = true;
-
-honrarPadres && guardarShabbat && diezmar && amorAPapa
-  ? 'Estas amando a Yahweh'
-  : 'No estas amando a Yahweh';
